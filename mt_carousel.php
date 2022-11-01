b936822d76a222f9bc724bb3c9b7ee7601638c47
martint <mart.tremblay.mt5@gmail.com>
2022-10-31 20:10:40 -0400

    - .git file corrupted, récupération de tous les fichiers - dépôt original github (w31-extension) renommé (w31-extension-old) - création d'un nouveau dépôt sur github : w31-extension - commit et push dans le nouveau dépôt

---
+++ mt_carousel.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @package Carroussel
+ * version 1.0.0
+ */
+/*
+Plugin Name: MT_carroussel
+Version: 1.0.0
+*/
+
+/*
+Fonctions utiles
+----------------
+filemtime()  // retourne en milliseconde le temps de la dernière sauvegarde
+plugin_dir_path() // retourne le chemin du répertoire du plugin
+__FILE__ // le fichier en train de s'exécuter
+wp_enqueue_style() // Intègre le link:css dans la page
+wp_enqueue_script() // intègre le script dans la page
+wp_enqueue_scripts // le hook
+ * */
+
+function emc_enqueue()
+{
+    $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
+    $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
+    wp_enqueue_style("mtc_style_carrousel",
+        plugins_dir_url(__FILE__) . "style.css",
+        array(),
+        $version_css,
+        false
+    );
+    wp_enqueue_script("mtc_js_carrousel",
+            plugins_dir_url(__FILE__) . "js/carrousel.js",
+            array(),
+            $version_js,
+            true
+    );
+
+    add_action("wp_enqueue_scripts", "emc_enqueue");
+
+}
+
+function generer_boite()
+{
+    $contenu = "
+	<style>
+		.carousel {
+			border: 2px solid black;
+			background-color: darkseagreen;
+			width: 200px;
+			height: 200px;
+		}
+	</style>
+	<div class='carrousel'>Re-voici l'extension Carrousel</div>";
+    return $contenu;
+}
+
+add_shortcode("mt_carrousel", "generer_boite");
