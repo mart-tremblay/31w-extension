af9cde2678bd96f6ee6f6ebfa7a14b7eccc6bbb4
martint <mart.tremblay.mt5@gmail.com>
2022-10-31 22:47:57 -0400

    - fixe du modale par incorporation du script "carrousel.js" de l'enseignant Eddy Martin

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,3 +1,94 @@
 (function () {
-	console.log("Carrousel - test carrousel.js");
-}());
+	console.log("carrousel");
+	/* ----------------------------------------- Initialisation des compteurs */
+	let index = 0;
+	let dernierIndex = -1;
+
+	/* ---------------------------------------- Les éléments du carrousel */
+	/* Le conteneur principal du carrousel */
+	let elmCarrousel = document.querySelector(".carrousel");
+	/* Bouton temporaire pour ouvrir le carrousel */
+	let elmBtnModale = document.querySelector(".btn_modale");
+	/* Bouton de fermeture du carrousel  le X de fermeture */
+	let elmBtnModaleFermer = document.querySelector(".btn_fermer");
+	/* figure qui contiendra l'ensemble des images du carrousel */
+	let elmCarrousel__figure = document.querySelector(".carrousel__figure");
+	/* le formulaire qui contiendra l'ensemble des boutons radio */
+	let elmCarrousel__form = document.querySelector(".carrousel__form");
+
+	/* ---------------------------------------- Les éléments de la galerie */
+	/* Le conteneur principal de la galerie */
+	let elmGalerie = document.querySelector(".galerie");
+	let elmGalerieImg = document.querySelectorAll(".galerie figure img");
+
+	/* ----------------------------- Étape 1 parcourir les images de la galerie */
+
+	for (const elmImg of elmGalerieImg) {
+		console.log(elmImg.getAttribute("src"));
+
+		ajouter_img_carrousel(elmImg);
+		ajouter_radio_carrousel();
+		/* écouteur sur les images de la galerie */
+		elmImg.addEventListener("mousedown", function () {
+			console.log("galerie");
+			console.log("elmImg.dataset.index" + this.dataset.index);
+			elmCarrousel.classList.add("carrousel--ouvrir");
+			elmCarrousel__figure.children[this.dataset.index].classList.add(
+				"carrousel__figure__img--activer"
+			);
+			elmCarrousel__form.children[this.dataset.index].checked = true;
+			console.log(index);
+			dernierIndex = this.dataset.index;
+		});
+	}
+	/**
+	 * Ajoute une image dans le carrousel
+	 * @param {*} elmImg  une image de la galerie
+	 */
+	function ajouter_img_carrousel(elmImg) {
+		// elmImg représente une image de la galerie */
+		elmImg.dataset.index = index;
+		let elmCarrousel__figure__img = document.createElement("img");
+		elmCarrousel__figure__img.setAttribute("src", elmImg.getAttribute("src"));
+		elmCarrousel__figure__img.classList.add("carrousel__figure__img");
+		elmCarrousel__figure__img.dataset.index = index;
+		elmCarrousel__figure.appendChild(elmCarrousel__figure__img);
+	}
+	/**
+	 * Ajoute un radio bouton dans le carrousel
+	 */
+	function ajouter_radio_carrousel() {
+		let elmCarrousel__form__radio = document.createElement("input");
+		elmCarrousel__form__radio.setAttribute("name", "carrousel__form__radio");
+		elmCarrousel__form__radio.setAttribute("class", "carrousel__form__radio");
+		elmCarrousel__form__radio.setAttribute("type", "radio");
+		elmCarrousel__form__radio.dataset.index = index;
+		index++;
+		elmCarrousel__form.appendChild(elmCarrousel__form__radio);
+		/* ------------------------ écouteur sur radio pour afficher une nouvelle image */
+		elmCarrousel__form__radio.addEventListener("mousedown", function () {
+			console.log(this.dataset.index);
+
+			if (dernierIndex != -1) {
+				elmCarrousel__figure.children[dernierIndex].classList.remove(
+					"carrousel__figure__img--activer"
+				);
+			}
+
+			elmCarrousel__figure.children[this.dataset.index].classList.add(
+				"carrousel__figure__img--activer"
+			);
+			console.log(index);
+			dernierIndex = this.dataset.index;
+		});
+	}
+
+	elmBtnModale.addEventListener("mousedown", function () {
+		console.log("bouton boîte modale");
+		elmCarrousel.classList.add("carrousel--ouvrir");
+	});
+	//////////////////////////////////////////////////
+	elmBtnModaleFermer.addEventListener("mousedown", function () {
+		elmCarrousel.classList.remove("carrousel--ouvrir");
+	});
+})();
--- mt_carousel.php
+++ mt_carousel.php
@@ -1,55 +1,48 @@
 <?php
 /**
- * @package Carroussel
+ * package Carrousel
  * version 1.0.0
  */
 /*
-Plugin Name: MT_carroussel
+Plugin Name: mt_carrousel
 Version: 1.0.0
 */
-
 /*
-Fonctions utiles
-----------------
 filemtime()  // retourne en milliseconde le temps de la dernière sauvegarde
 plugin_dir_path() // retourne le chemin du répertoire du plugin
 __FILE__ // le fichier en train de s'exécuter
 wp_enqueue_style() // Intègre le link:css dans la page
 wp_enqueue_script() // intègre le script dans la page
 wp_enqueue_scripts // le hook
- * */
+*/
 
 function emc_enqueue()
 {
-    $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
-    $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
-    wp_enqueue_style("mtc_style_carrousel",
-        plugins_dir_url(__FILE__) . "style.css",
-        array(),
-        $version_css,
-        false
-    );
-    wp_enqueue_script("mtc_js_carrousel",
-            plugins_dir_url(__FILE__) . "js/carrousel.js",
-            array(),
-            $version_js,
-            true
-    );
+	$version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
+	$version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
 
-    add_action("wp_enqueue_scripts", "emc_enqueue");
-
+	wp_enqueue_style("mtc_style_carrousel",
+		plugin_dir_url(__FILE__) . "style.css",
+		array(),
+		$version_css,
+		false);
+	wp_enqueue_script("mtc_js_carrousel",
+		plugin_dir_url(__FILE__) . "js/carrousel.js",
+		array(),
+		$version_js,
+		true);
 }
 
-function generer_boite()
-{
-    $contenu = "
+add_action('wp_enqueue_scripts', 'emc_enqueue');
+
+function genere_boite(){
+	$contenu = "
     <button class='btn_modale'>boîte modale</button>
     <div class='carrousel'>
         <button class='btn_fermer'>X</button>
         <figure class='carrousel__figure'></figure>
         <form class='carrousel__form'></form>
     </div>";
-    return $contenu;
+	return $contenu;
 }
-
-add_shortcode("mt_carrousel", "generer_boite");
+add_shortcode('mt_carrousel', 'genere_boite');
